309917c192cc892dc51fe68ca4b45d8eb4b8a270
JET74 <tobbe.petersson@gmail.com>
2021-04-11 19:58:23 +0200

    Started creating a screen scanner (#2)
    Co-authored-by: Tobias Petersson <tobias.petersson@sigma.se>

---
+++ PoE.BuffWatcher.Capture/Extensions/RectExtensions.cs
@@ -0,0 +1,16 @@
+﻿using PoE.BuffWatcher.Capture.Structs;
+
+namespace PoE.BuffWatcher.Capture.Extensions
+{
+    public static class RectExtensions
+    {
+        public static bool IsZeroRect(this Rect rect)
+        {
+            return
+                rect.Bottom == 0 &&
+                rect.Left == 0 &&
+                rect.Right == 0 &&
+                rect.Top == 0;
+        }
+    }
+}
---
+++ PoE.BuffWatcher.Capture/ImageCapture.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using PoE.BuffWatcher.Capture.Imports;
+
+namespace PoE.BuffWatcher.Capture
+{
+    public class ImageCapture
+    {
+        private readonly WindowHandler _windowHandler;
+
+        public ImageCapture(WindowHandler windowHandler)
+        {
+            _windowHandler = windowHandler;
+        }
+
+        public Image GetBitmapFromGameWindow()
+        {
+            var gameWindow = _windowHandler.GetGameWindow();
+            if (gameWindow == IntPtr.Zero)
+            {
+                return null;
+            }
+
+            var gameWindowDc = _windowHandler.GetGameWindowDC();
+            var windowRect = _windowHandler.GetWindowRect();
+            int width = windowRect.Right - windowRect.Left;
+            int height = windowRect.Bottom - windowRect.Top;
+
+            IntPtr hdcDest = GDI32.CreateCompatibleDC(gameWindowDc);
+            IntPtr hBitmap = GDI32.CreateCompatibleBitmap(gameWindowDc, width, height);
+            IntPtr hOld = GDI32.SelectObject(hdcDest, hBitmap);
+            GDI32.BitBlt(hdcDest, 0, 0, width, height, gameWindowDc, 0, 0, GDI32.SRCCOPY);
+            GDI32.SelectObject(hdcDest, hOld);
+            GDI32.DeleteDC(hdcDest);
+            User32.ReleaseDC(gameWindow, gameWindowDc);
+            
+            Image image = Image.FromHbitmap(hBitmap);
+            GDI32.DeleteObject(hBitmap);
+
+            return image;
+        }
+
+        public void SaveScreenShot(string fileName, ImageFormat format = null)
+        {
+            format ??= ImageFormat.Bmp;
+
+            var bitmap = GetBitmapFromGameWindow();
+            if (bitmap == null)
+            {
+                Console.WriteLine("Unable to find game window");
+                return;
+            }
+
+            bitmap.Save(fileName, format);
+        }
+    }
+}
+
